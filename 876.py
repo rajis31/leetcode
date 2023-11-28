9e07a6f78aa8b0bf5a0eaab0f146fde92c212a9d
Raji <rajsolan91@gmail.com>
2023-11-27 21:08:17 -0800

    Create 876.py

---
+++ 876.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        curr  = head
+        nodes = []
+
+        while curr:
+            nodes.append(curr)
+            curr = curr.next
+        
+        
+        return nodes[len(nodes)//2]
+    
+        
+
+        
